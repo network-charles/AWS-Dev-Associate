a410c1e18b281fd42dee3c6934883ccd96fa9d31
Charles Uneze <charlesniklaus@gmail.com>
2023-12-29 04:44:33 +0100

    Create data.tf

---
+++ Lab 2/data.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "ecr_codecommit" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecr:CreateRepository",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+      "codecommit:GitPull"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
