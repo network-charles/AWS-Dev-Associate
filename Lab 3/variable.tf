be518620ddcbd80ed2e0bd1b44ff2cedd425f0aa
Charles Uneze <charlesniklaus@gmail.com>
2024-01-10 15:38:19 +0100

    Create variable.tf

---
+++ Lab 3/variable.tf
@@ -0,0 +1,19 @@
+variable "ubuntu" {
+  type    = string
+  default = "ami-0505148b3591e4c07"
+}
+
+variable "key_name" {
+  type    = string
+  default = ""
+}
+
+variable "user_name" {
+  type    = string
+  default = ""
+}
+
+variable "email" {
+  type    = string
+  default = ""
+}
