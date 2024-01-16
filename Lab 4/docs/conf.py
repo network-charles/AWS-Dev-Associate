0a3b58a1b38bde6df445f6d23aa4e3c6aa3ea916
Charles Uneze <charlesniklaus@gmail.com>
2024-01-17 00:16:26 +0100

    Update conf.py

--- Lab 4/docs/conf.py
+++ Lab 4/docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Docker ARP Analysis Guide'
+project = 'Docker ARP Analysis Documentation'
 copyright = '2024, Charles Uneze'
 author = 'Charles Uneze'
 release = 'v0.1'
