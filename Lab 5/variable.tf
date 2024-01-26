97b5ca1a164a0b62a5319fcce997e978ced29341
Charles Uneze <charlesniklaus@gmail.com>
2024-01-26 19:38:27 +0100

    Update variable.tf

--- Lab 5/variable.tf
+++ Lab 5/variable.tf
@@ -1,9 +1,9 @@
 variable "user_name" {
   type    = string
-  default = "network-charles"
+  default = ""
 }
 
 variable "email" {
   type    = string
-  default = "networkcharles@outlook.com"
+  default = ""
 }
